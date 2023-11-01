038825f81779c7f5109b6d57b08afa9a5c89937f
Nguyen Duy Manh <duym63094@gmail.com>
2023-11-01 15:21:15 +0700

    second

--- src/main/java/com/minhchan/testci/main/Main.java
+++ src/main/java/com/minhchan/testci/main/Main.java
@@ -15,6 +15,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("5!= " + MathUtil.getFactorial(5));
-        System.out.printlsfoidshgneifbuvbifedckjn("666");
+        System.out.println("666");
     }
 }
